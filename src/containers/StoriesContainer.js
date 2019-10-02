877429cd78c83a9afbfeb64a3a5c5eb2930e2a30
Louis <louis@Louiss-Mac-mini.local>
2019-10-02 16:38:18 -0400

    Added Story.js and styled-components

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
-import { getStoryIds, getStory } from '../services/hnApi';
+import { getStoryIds } from '../services/hnApi';
+import { Story } from '../components/Story';
+import {
+  GlobalStyle,
+  StoriesContainerWrapper
+} from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds().then(data => setStoryIds(data));
-    getStory(21133181).then(data => console.log(data));
   }, []);
 
   return (
-    <p>{JSON.stringify(storyIds)}</p>
-
-  )
-}
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.slice(0, 10).map(storyId => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
+};
